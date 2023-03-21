25ec6d3e5d0cab89c13744f52bbe036088ebaf36
veinarous1 <veinarous@gmail.com>
2023-03-20 19:55:47 -0700

    task4 updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include <stdioh>
+#include<stdio.h>
 /**
- * main - My first c program that prints line using the puts function
- * Return: 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
