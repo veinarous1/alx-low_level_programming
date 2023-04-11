11a95bcb298efae05851bf22681e169dff47ba1b
veinarous1 <veinarous@gmail.com>
2023-04-10 17:29:34 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ * prints a program followed by a new line
+ *
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n",argc -1);
+	return 0;
+	}
